95e900db2a1ca7ff86187b7ecff8de11c3a2a6a5
Abhishek Sangwan <abhisheksangwan1994@gmail.com>
2016-04-25 07:52:00 +0530

    list adapter code refactored and new activity weather report

--- app/src/main/java/com/example/linux/weatherforecast/CityWeatherForecast.java
+++ app/src/main/java/com/example/linux/weatherforecast/CityWeatherForecast.java
@@ -2,20 +2,51 @@ package com.example.linux.weatherforecast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.example.linux.weatherforecast.Data.Data;
 
 public class CityWeatherForecast extends AppCompatActivity {
     private Data data;
+    ListView cityDailyWeatherList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_weather_forecast);
+        getDataFromIntent();
+        initializeViews();
+        showCityWeatherDetails();
+        setEventsForViews();
     }
+
+    private void showCityWeatherDetails(){
+        cityDailyWeatherList.setAdapter(new ListAdapter(this, data.getList(), "DAILY_REPORT"));
+
+    }
+
+    private void initializeViews(){
+        cityDailyWeatherList = (ListView) findViewById(R.id.city_daily_forecast_list);
+    }
+
+    private void setEventsForViews(){
+        cityDailyWeatherList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("Notes", "item clicked");
+                //Data data = c.getCitiesData().get(position);
+                //Navigate.toCityWeatherForecast(CitiesDetails.this, data);
+            }
+        });
+    }
+
+
 
 
     private void getDataFromIntent(){
         Bundle b = getIntent().getExtras();
-         data = b.getParcelable("citiesData");
+         data = b.getParcelable("data");
     }
 }
--- app/src/main/java/com/example/linux/weatherforecast/ListAdapter.java
+++ app/src/main/java/com/example/linux/weatherforecast/ListAdapter.java
@@ -10,8 +10,12 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.example.linux.weatherforecast.Data.CitiesData;
+import com.example.linux.weatherforecast.Data.DailyReport;
 import com.example.linux.weatherforecast.Data.Data;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 
 /**
@@ -21,6 +25,10 @@ public class ListAdapter extends BaseAdapter{
     Context context;
     LinkedList<Data> citiesData;
     LayoutInflater inflater;
+    String from = new String("");
+    LinkedList<DailyReport> dailyReports;
+    ViewHolder viewHolder;
+    DateHolder dateHolder;
 
     public ListAdapter(Context context, LinkedList<Data> citiesData){
         Log.i("Notes", context +  "");
@@ -28,14 +36,26 @@ public class ListAdapter extends BaseAdapter{
         this.citiesData = citiesData;
     }
 
+    public ListAdapter(Context context, LinkedList<DailyReport> dailyReports, String from){
+        this.context = context;
+        this.from = from;
+        this.dailyReports = dailyReports;
+    }
+
     @Override
     public int getCount() {
-        return citiesData.size();
+        if (from.equals("DAILY_REPORT"))
+            return  dailyReports.size();
+        else
+           return citiesData.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return citiesData.get(position);
+        if (from.equals("DAILY_REPORT"))
+            return dailyReports.size();
+        else
+            return citiesData.get(position);
     }
 
     @Override
@@ -47,24 +67,60 @@ public class ListAdapter extends BaseAdapter{
 
     @Override
     public View getView(int i, View view, ViewGroup parent) {
-        ViewHolder viewHolder;
+        viewHolder = new ViewHolder();
+        dateHolder = new DateHolder();
         if (view == null){
             inflater = (LayoutInflater) context.getSystemService(Service.LAYOUT_INFLATER_SERVICE);
-            view = inflater.inflate(R.layout.cities_weather_detail, parent, false);
-            viewHolder = new ViewHolder();
-            viewHolder.cityName = (TextView) view.findViewById(R.id.city_name);
-            viewHolder.temperature = (TextView) view.findViewById(R.id.todays_temp);
-            Data data = citiesData.get(i);
-            viewHolder.cityName.setText(data.getCity().getName());
-            viewHolder.temperature.setText(data.getList().get(0).getTemp().getMax() + "");
-            view.setTag(viewHolder);
+            if (from.equals("DAILY_REPORT")) {
+                view = inflater.inflate(R.layout.daily_report_date, parent, false);
+                view.setTag(dateHolder);
+            }
+            else {
+                view = inflater.inflate(R.layout.cities_weather_detail, parent, false);
+                view.setTag(viewHolder);
+            }
         }
+
+        return renderView(view, i);
+    }
+
+    private View renderView(View view, int i){
+        return from.equals("DAILY_REPORT") ? showCityDailyReport(i, view) : showCitiesDetails(i, view);
+    }
+
+    private View showCityDailyReport(int i,View view){
+        dateHolder.date = (TextView) view.findViewById(R.id.date);
+        DailyReport dailyReport = dailyReports.get(i);
+        Date date = new Date(dailyReport.getDt() * 1000);
+        Log.i("Notes", dailyReport.getDt() + "");
+        dateHolder.date.setText(date.toString());
+        view.setTag(dateHolder);
         return view;
     }
 
 
-    class ViewHolder{
-        TextView cityName;
-        TextView temperature;
+    private View showCitiesDetails(int i, View view){
+        viewHolder.leftText = (TextView) view.findViewById(R.id.left_text);
+        viewHolder.rightText = (TextView) view.findViewById(R.id.right_text);
+        Data data = citiesData.get(i);
+        viewHolder.leftText.setText(data.getCity().getName());
+        viewHolder.rightText.setText(data.getList().get(0).getTemp().getMax() + "");
+        view.setTag(viewHolder);
+        return view;
     }
+
+
+
+
+
+    class ViewHolder{
+        TextView leftText;
+        TextView rightText;
+    }
+
+    class DateHolder{
+        TextView date;
+    }
+
+
 }
